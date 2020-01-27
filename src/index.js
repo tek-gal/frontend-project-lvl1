ece5cc42b910a936f9bfb5270ca4d23e6542acfc
tek-gal <tek-gal@yandex.ru>
2020-01-27 23:23:04 +0300

    it works!!!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const hello = () => {
+export const hello = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!\n`);
   return name;
